a25d7d170ed9783744f9106e9a629c1b0aad0c37
LM <Sergey>
2020-12-05 10:34:11 +0200

    laba 5.1

--- php/data/bd.php
+++ php/data/bd.php
@@ -1,6 +1,13 @@
 <?php
 include ("php/bd/bd.php");
-$query  = "SELECT * FROM info WHERE id=2";
-$result = $db->query($query);
-$myrow = $result ->fetch();
+try {
+    include("php/bd/bd.php");
+    $query = "SELECT * FROM info WHERE id=2";
+    $result = $db->query($query);
+    $myrow = $result->fetch();
+}
+catch (Throwable $e)
+{
+    echo "Captured Throwable: " . $e->getMessage() . PHP_EOL;
+}
 ?>
